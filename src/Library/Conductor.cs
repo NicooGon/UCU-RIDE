a0e60f0d97f17f6ad8e99f223183c92b490181fa
Nicolas Gonzalez <nicolasgonzalezgutierrez2017@gmail.com>
2023-10-17 09:51:43 -0300

    Arreglos

--- src/Library/Conductor.cs
+++ src/Library/Conductor.cs
@@ -4,6 +4,13 @@ namespace Ucu.Poo.Twitter;
 
 public class Conductor : Persona
 {
+    public Conductor(string name, string surname, string CI, string calificacion)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.CI = CI;
+        this.Calificacion = Calificacion;
+    }
     public void Biografia()
     {
         Console.WriteLine($"Biografia del conductor\nNombre : {this.Name}\nApellido : {this.Surname}\nCedula : {this.CI}\nCalificacion del conductor : {this.Calificacion}");
