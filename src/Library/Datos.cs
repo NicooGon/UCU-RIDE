9e5ad90fd1ba71531446cafe47bd0b51d6d7fcd2
Nicolas Gonzalez <nicolasgonzalezgutierrez2017@gmail.com>
2023-10-17 09:17:08 -0300

    datos

---
+++ src/Library/Datos.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PII_Herencia;
+
+public class Datos
+{
+    string name {get; set;}
+    string surname {get; set;}
+    string CI {get;set;}
+    string Clasificacion {get;set;}
+}
